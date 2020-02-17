9cb8e1eb78568dafb1d661c223d6e8e6fadf2519
Joseph Shaferman <joseph.shaferman@gmail.com>
2020-02-17 13:39:14 +0200

    print github token

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const token = core.getInput('token');
+  console.log('token:', token);
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
