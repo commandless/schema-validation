95b7dd6e1afdeca34f24f4cbf3ef46e581102957
Joseph Shaferman <joseph.shaferman@gmail.com>
2020-02-17 15:34:09 +0200

    get commits

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  const token = core.getInput('github-token');
-  console.log('token:', typeof token, token.length);
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function validatePr() {
+  try {
+    const token = core.getInput('github-token');
+    console.log('token:', typeof token, token.length);
+  
+    const octokit = new github.GitHub(token);
+  
+    const x = await octokit.commits.get({
+      sha: github.context.payload.commits[0].id
+    })
+
+    console.log("==========")
+    console.log(x)
+    console.log("==========")
+    
+    core.setOutput("time", time);
+  
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+validatePr()
